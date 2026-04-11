41126152c8519f5322d3e359a87fd15b539ada11
Oszau <oszau@mail.ru>
2026-04-11 22:07:22 +0300

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -751,6 +751,7 @@ struct nvram_pair router_defaults[] = {
 	{ "fw_mac_drop", "0" },
 	{ "nf_nat_type", "2" },
 	{ "nf_nat_loop", "1" },
+    { "nf_nat_snat", "1" },
 #if (BOARD_RAM_SIZE > 128)
 	{ "nf_max_conn", "32768" },
 #elif (BOARD_RAM_SIZE > 32)
